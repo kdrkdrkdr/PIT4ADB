e0254db993286ce9670f3b98f0b4a4d5716cc331
kdrkdrkdr <kdrhacker1234@gmail.com>
2021-12-15 14:50:27 +0900

    init2

--- client.py
+++ client.py
@@ -12,7 +12,13 @@ import re, os, glob
 
 d = ui.connect()
 
-ext_storage = re.sub('[\t\n\r]', '', str(subprocess.check_output("utils\\adb.exe shell echo $EXTERNAL_STORAGE", shell=True).decode('utf-8')))
+adb_location = 'nox_adb'
+
+ext_storage = re.sub('[\t\n\r]', '', str(subprocess.check_output(f"{adb_location} shell echo $EXTERNAL_STORAGE", shell=True).decode('utf-8')))
+
+push_ori_dir = f'{ext_storage}/PIT4ADB_TEMP'
+pull_transed_dir = f'{ext_storage}/papago'
+
 
 
 class RunTranslate(QThread):
@@ -29,15 +35,18 @@ class RunTranslate(QThread):
 
     def run(self):
 
-        d.app_start('com.naver.labs.translator', stop=True)
+        os.system(f'{adb_location} shell rm -rf {push_ori_dir}')
+        os.system(f'{adb_location} shell mkdir {push_ori_dir}')
+        os.system(f'{adb_location} shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{ext_storage}')
         sleep(3)
-        d.xpath('//*[@resource-id="com.naver.labs.translator:id/btn_ocr"]').click()
-        sleep(1)
 
+        d.app_start('com.naver.labs.translator', stop=True)
+        sleep(5)
+        d.xpath('//*[@resource-id="com.naver.labs.translator:id/btn_ocr"]').click()
         
 
-
         src_dir = self.window.transed_file_dir.text().replace('/', '\\')
+        # src_dir = 'C:\\Users\\power\\Desktop\\Project\\Dev\\PIT4ADB\\MyImages'
 
         imageTypes = ('jpg', 'png')
         imageList = []
@@ -51,7 +60,6 @@ class RunTranslate(QThread):
 
 
         for i, j in enumerate(imageList):
-
             srcLang = d.xpath('//*[@resource-id="com.naver.labs.translator:id/source_language_text"]')
             fLang = self.window.fromLang.currentText()
 
@@ -73,29 +81,47 @@ class RunTranslate(QThread):
             src_path = f'{src_dir}\\{os.path.basename(j)}'
             dst_dir = f'{src_dir}\\PAPAGO_TRANSLATED'
 
-            d.push(src=src_path, dst=f'{ext_storage}/Pictures/')
-            d.shell(f'am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{ext_storage}/Pictures')
 
+            os.system(f'{adb_location} push {src_path} {push_ori_dir}/{os.path.basename(j)}')
+            os.system(f'{adb_location} shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{push_ori_dir}/{os.path.basename(j)}')
+            # sleep(2)
             d.xpath('//*[@resource-id="com.naver.labs.translator:id/btn_gallery"]').click()
-            d.xpath('//*[@resource-id="com.android.documentsui:id/sub_menu"]').click()
-            d.xpath('//*[@resource-id="com.android.documentsui:id/dir_list"]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ImageView[1]').click()
-            d.xpath('//*[@resource-id="com.android.documentsui:id/option_menu_search"]').click()
+
+
+            d.xpath('//*[@content-desc="루트 표시"]').click()
+            sleep(1)
+            d.xpath('//*[@text="이미지"]').click()
+            sleep(1)
+            d.xpath('//*[@text="PIT4ADB_TEMP"]').click()
+
+            try:
+                k = d.xpath('//*[@resource-id="com.android.documentsui:id/menu_grid"]')
+                if k.exists:
+                    print(k.click())
+            except:
+                print("그냥 지나가라")
+
+            sleep(1)
+            d.xpath('//*[@resource-id="com.android.documentsui:id/dir_list"]/android.widget.RelativeLayout[1]/android.widget.RelativeLayout[1]').click()
 
             while not d(resourceId="com.naver.labs.translator:id/btn_save").exists: pass
             d(resourceId="com.naver.labs.translator:id/btn_save").click()
             d.xpath('//*[@text="저장하기"]').click()
 
-            time_stamp = re.sub('[\D]', '', str(subprocess.check_output(f"utils\\adb.exe shell ls -l {ext_storage}/Pictures/papago_*", shell=True)).split(' ')[-1])
-            transed_jpg = f'{ext_storage}/Pictures/papago_{time_stamp}.jpg'
+            time_stamp = re.sub('[\D]', '', str(subprocess.check_output(f"{adb_location} shell ls -l {pull_transed_dir}/papago_*", shell=True)).split(' ')[-1])
+            transed_jpg = f'{pull_transed_dir}/papago_{time_stamp}.jpg'
 
 
             if not os.path.isdir(dst_dir): os.mkdir(dst_dir)
             
-            d.pull(transed_jpg, f'{dst_dir}\\transed_{os.path.basename(j)}')
+            os.system(f'{adb_location} pull {transed_jpg} {dst_dir}\\transed_{os.path.basename(j)}')
 
-            d.shell(f'rm -rf {transed_jpg} {ext_storage}/Pictures/{os.path.basename(j)}')
-            d.shell(f'am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{ext_storage}/Pictures')
+            os.system(f'{adb_location} shell rm -rf {transed_jpg} {push_ori_dir}/{os.path.basename(j)}')
 
+            os.system(f'{adb_location} shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{transed_jpg}')
+            os.system(f'{adb_location} shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://{push_ori_dir}/{os.path.basename(j)}')
+            
+            # sleep(2)
             d.xpath('//*[@resource-id="com.naver.labs.translator:id/btn_back"]').click()
 
             self.window.f_log_browser.append(f'{dst_dir}\\transed_{os.path.basename(j)} 에 저장되었습니다.')
--- initialize.bat
+++ initialize.bat
@@ -1,4 +1,5 @@
-utils\adb install -r utils\ATX_base.apk
-utils\adb push utils\atx-agent /data/local/tmp
-utils\adb shell chmod 755 /data/local/tmp/atx-agent
-utils\adb shell /data/local/tmp/atx-agent server -d
+nox_adb install -r utils\Papago_base.apk
+nox_adb install -r utils\ATX_base.apk
+nox_adb push utils\atx-agent /data/local/tmp
+nox_adb shell chmod 755 /data/local/tmp/atx-agent
+nox_adb shell /data/local/tmp/atx-agent server -d
